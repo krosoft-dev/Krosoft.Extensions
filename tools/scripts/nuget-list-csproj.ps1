8408464ad63c3d51780224f8cf2e2391e788609e
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-11-23 09:26:41 +0100

    Clean

--- tools/scripts/nuget-list-csproj.ps1
+++ tools/scripts/nuget-list-csproj.ps1
@@ -1,12 +1,26 @@
 (Get-ChildItem ./src -Filter *.csproj -Recurse) | ForEach-Object {
   Write-Host =========== $_.BaseName
-  Get-Content $_.FullName `
+ 
+  $fileName = "./temp/nuget-$($_.BaseName)-pipeline.yml"
+  New-Item $fileName -Force #-ErrorAction SilentlyContinue
+  Set-Content $fileName $content
+
+  $content = Get-Content $_.FullName `
   | Find "<ProjectReference Include=" `
   | ForEach-Object { $_ -replace '<ProjectReference Include=', '' -replace '/>', '' }  `
   | Sort-Object -Unique  `
-  | Split-Path -Leaf  
+  | Split-Path -Leaf   `
+  | ForEach-Object {$_.replace('.csproj"','')} 
+  
+  Set-Content $fileName $content
+  Write-Host $content
   Write-Host ===========
   Write-Host  
 }
 
  
+
+# New-Item D:\temp\test\test.txt
+ 
+
+# Set-Content D:\temp\test\test.txt 'Welcome to TutorialsPoint'
