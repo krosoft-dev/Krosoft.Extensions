7252cec5473c07759c683579a649adc5c545e9cf
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-12-11 17:31:14 +0100

    Ajout des librairies de gestion du cache

---
+++ src/Krosoft.Extensions.Cache.Distributed.Redis/Services/RedisConnectionFactory.cs
@@ -0,0 +1,23 @@
+﻿using Krosoft.Extensions.Cache.Distributed.Redis.Interfaces;
+using Krosoft.Extensions.Core.Tools;
+using Microsoft.Extensions.Configuration;
+using StackExchange.Redis;
+
+namespace Krosoft.Extensions.Cache.Distributed.Redis.Services;
+
+internal class RedisConnectionFactory : IRedisConnectionFactory
+{
+    private readonly Lazy<IConnectionMultiplexer> _connection;
+
+    public RedisConnectionFactory(IConfiguration configuration)
+    {
+        Guard.IsNotNull(nameof(configuration), configuration);
+        var connectionString = configuration["ConnectionStrings:Redis"];
+        if (connectionString != null)
+        {
+            _connection = new Lazy<IConnectionMultiplexer>(() => ConnectionMultiplexer.Connect(connectionString));
+        }
+    }
+
+    public IConnectionMultiplexer Connection => _connection.Value;
+}
