c93ff5b70c67db5e86fefc4c8f03e19688a17266
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-12-11 17:56:20 +0100

    Utilisation UTF8

--- samples/Krosoft.Extensions.Samples.DotNet8.Api/Handlers/Queries/LogicielsExportPdfQueryHandler.cs
+++ samples/Krosoft.Extensions.Samples.DotNet8.Api/Handlers/Queries/LogicielsExportPdfQueryHandler.cs
@@ -28,7 +28,7 @@ public class LogicielsExportPdfQueryHandler : IRequestHandler<LogicielsExportPdf
         await Task.Delay(2000, cancellationToken);
 
         var assembly = typeof(AddresseFactory).Assembly;
-        var encoding = Encoding.Latin1;
+        var encoding = Encoding.UTF8;
 
         var pdf1 = FileHelper.ReadAsStream(assembly, "sample1.pdf", encoding);
         var pdf2 = FileHelper.ReadAsStream(assembly, "sample2.pdf", encoding);
