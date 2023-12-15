a894b2ea2d48a58b818b580b0bf0ec7b084e4ebe
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-12-15 17:55:35 +0100

    Ajout TUs

--- src/Krosoft.Extensions.Core/Helpers/Base64Helper.cs
+++ src/Krosoft.Extensions.Core/Helpers/Base64Helper.cs
@@ -9,16 +9,26 @@ public static class Base64Helper
     /// </summary>
     /// <param name="base64EncodedData">texte en base 64</param>
     /// <returns>le texte en string</returns>
-    public static string Base64ToString(string? base64EncodedData)
+    public static string? Base64ToString(string? base64EncodedData)
     {
-        var encodedDataAsBytes = Convert.FromBase64String(base64EncodedData);
-        var plainText = Encoding.UTF8.GetString(encodedDataAsBytes, 0, encodedDataAsBytes.Length);
-        return plainText;
+        if (base64EncodedData != null)
+        {
+            var encodedDataAsBytes = Convert.FromBase64String(base64EncodedData);
+            var plainText = Encoding.UTF8.GetString(encodedDataAsBytes, 0, encodedDataAsBytes.Length);
+            return plainText;
+        }
+
+        return null;
     }
 
-    public static string StringToBase64(string? plainText)
+    public static string? StringToBase64(string? plainText)
     {
-        var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
-        return Convert.ToBase64String(plainTextBytes);
+        if (plainText != null)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+
+        return null;
     }
 }
