f6af758ca9e74801074a9e15bb0c90dda1987f6d
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-12-15 17:52:46 +0100

    Ajout TUs

--- src/Krosoft.Extensions.Core/Helpers/RegexHelper.cs
+++ src/Krosoft.Extensions.Core/Helpers/RegexHelper.cs
@@ -1,7 +1,4 @@
-﻿using System.Text.RegularExpressions;
-using Krosoft.Extensions.Core.Models.Exceptions;
-
-namespace Krosoft.Extensions.Core.Helpers;
+﻿namespace Krosoft.Extensions.Core.Helpers;
 
 public static class RegexHelper
 {
---
+++ tests/Krosoft.Extensions.Core.Tests/Helpers/Base64HelperTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Krosoft.Extensions.Core.Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+
+namespace Krosoft.Extensions.Core.Helpers.Tests
+{
+    [TestClass()]
+    public class Base64HelperTests
+    {
+        [DataTestMethod]
+        [DataRow("SGVsbG8sIFdvcmxkIQ==", "Hello, World!")]
+        [DataRow("", "")]
+        [DataRow(null, null)]
+        public void Base64ToString_ShouldConvertBase64ToString(string base64EncodedData, string expectedPlainText)
+        {
+            // Act
+            string result = Base64Helper.Base64ToString(base64EncodedData);
+
+            // Assert
+            Check.That(result).IsEqualTo(expectedPlainText);
+        }
+    }
+}
