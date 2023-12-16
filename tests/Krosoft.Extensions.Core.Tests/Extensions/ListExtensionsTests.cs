b5d16434eb552200fd3e131e508da5e870173887
Kévin ROBERT <kevin.robert.kr@gmail.com>
2023-12-16 08:30:23 +0100

    Ajout TUs

--- tests/Krosoft.Extensions.Core.Tests/Extensions/ListExtensionsTests.cs
+++ tests/Krosoft.Extensions.Core.Tests/Extensions/ListExtensionsTests.cs
@@ -11,19 +11,6 @@ public class ListExtensionsTests
     private readonly string[] _data = { "apple", "orange", "banana" };
 
     [TestMethod]
-    public void AddRange_Integer_NotCheckContains()
-    {
-        // Arrange
-        var list = new List<int> { 1, 2, 3, 8, 9, 8, 9 };
-
-        // Act
-        list.AddRange(new[] { 3, 4, 5 }, false);
-
-        // Assert
-        Check.That(list).ContainsExactly(1, 2, 3, 8, 9, 8, 9, 3, 4, 5);
-    }
-
-    [TestMethod]
     public void AddRange_Integer_CheckContains()
     {
         // Arrange
@@ -37,13 +24,26 @@ public class ListExtensionsTests
     }
 
     [TestMethod]
+    public void AddRange_Integer_NotCheckContains()
+    {
+        // Arrange
+        var list = new List<int> { 1, 2, 3, 8, 9, 8, 9 };
+
+        // Act
+        list.AddRange(new List<int> { 3, 4, 5 }, false);
+
+        // Assert
+        Check.That(list).ContainsExactly(1, 2, 3, 8, 9, 8, 9, 3, 4, 5);
+    }
+
+    [TestMethod]
     public void AddRange_String_CheckContains()
     {
         // Arrange
-        var list = new List<string>(new[] { "apple", "aaa", "orange", "bbbb" });
+        var list = new List<string>( { "apple", "aaa", "orange", "bbbb" });
 
         // Act
-        list.AddRange(new[] { "orange", "banana", "bbbb" }, true);
+        list.AddRange(new List<string> { "orange", "banana", "bbbb" }, true);
 
         // Assert
         Check.That(list).ContainsExactly("apple", "aaa", "orange", "bbbb", "banana");
@@ -57,6 +57,24 @@ public class ListExtensionsTests
 
         // Assert
         Check.That(result).IsEqualTo(new Dictionary<int, string> { { 5, "apple" }, { 6, "orange" } });
+    }
+
+    [TestMethod]
+    public void ToDictionaryWithModifier_ShouldReturnCorrectDictionary()
+    {
+        Check.ThatCode(() => new List<string> { "apple", "orange", "apple", "orange", "banana" }.ToDictionary(x => x.Length.ToString(), x => x.ToUpper(), false))
+             .Throws<ArgumentException>()
+             .WithMessage("An item with the same key has already been added. Key: 5");
+    }
+
+    [TestMethod]
+    public void ToDictionaryWithModifier_ShouldReturnCorrectDictionary_Distinct()
+    {
+        // Act
+        var result = new List<string> { "apple", "orange", "apple", "orange", "banana" }.ToDictionary(x => x.Length.ToString(), x => x.ToUpper(), true);
+
+        // Assert
+        Check.That(result).ContainsExactly(new Dictionary<string, string> { { "5", "apple" }, { "6", "orange" } });
     }
 
     [TestMethod]
@@ -75,23 +93,5 @@ public class ListExtensionsTests
 
         // Assert
         Check.That(result).IsEqualTo(new ReadOnlyDictionary<int, string>(new Dictionary<int, string> { { 5, "apple" }, { 6, "orange" } }));
-    }
-
-    [TestMethod]
-    public void ToDictionaryWithModifier_ShouldReturnCorrectDictionary_Distinct()
-    {
-        // Act
-        var result = new[] { "apple", "orange", "apple", "orange", "banana" }.ToDictionary(x => x.Length.ToString(), x => x.ToUpper(), true);
-
-        // Assert
-        Check.That(result).ContainsExactly(new Dictionary<string, string> { { "5", "apple" }, { "6", "orange" } });
-    }
-
-    [TestMethod]
-    public void ToDictionaryWithModifier_ShouldReturnCorrectDictionary()
-    {
-        Check.ThatCode(() => new[] { "apple", "orange", "apple", "orange", "banana" }.ToDictionary(x => x.Length.ToString(), x => x.ToUpper(), false))
-             .Throws<ArgumentException>()
-             .WithMessage("An item with the same key has already been added. Key: 5");
     }
 }
