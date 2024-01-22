0139c4710aece4273394b486a27e74ab0c8835b4
Kévin ROBERT <kevin.robert.kr@gmail.com>
2024-01-22 20:59:42 +0100

    Clean

--- src/Krosoft.Extensions.Blocking.Redis/Services/RedisBlockingStorageProvider.cs
+++ src/Krosoft.Extensions.Blocking.Redis/Services/RedisBlockingStorageProvider.cs
@@ -13,8 +13,8 @@ public class RedisBlockingStorageProvider : IBlockingStorageProvider
     }
 
     public virtual async Task<bool> IsSetAsync(string collectionKey,
-                                                    string key,
-                                                    CancellationToken cancellationToken)
+                                               string key,
+                                               CancellationToken cancellationToken)
     {
         var isExist = await _distributedCacheProvider.IsExistRowAsync(collectionKey, key, cancellationToken);
 
@@ -22,32 +22,32 @@ public class RedisBlockingStorageProvider : IBlockingStorageProvider
     }
 
     public virtual async Task<long> RemoveAsync(string collectionKey,
-                                                    ISet<string> keys,
-                                                    CancellationToken cancellationToken)
+                                                ISet<string> keys,
+                                                CancellationToken cancellationToken)
     {
         var number = await _distributedCacheProvider.DeleteRowsAsync(collectionKey, keys, cancellationToken);
         return number;
     }
 
     public virtual async Task SetAsync(string collectionKey,
-                                          IDictionary<string, string> entryByKey,
-                                          CancellationToken cancellationToken)
+                                       IDictionary<string, string> entryByKey,
+                                       CancellationToken cancellationToken)
     {
         await _distributedCacheProvider.SetRowAsync(collectionKey, entryByKey, cancellationToken);
     }
 
     public virtual async Task<bool> RemoveAsync(string collectionKey,
-                                                   string key,
-                                                   CancellationToken cancellationToken)
+                                                string key,
+                                                CancellationToken cancellationToken)
     {
         var isDelete = await _distributedCacheProvider.DeleteRowAsync(collectionKey, key, cancellationToken);
         return isDelete;
     }
 
     public virtual async Task SetAsync(string collectionKey,
-                                          string key,
-                                          string entry,
-                                          CancellationToken cancellationToken)
+                                       string key,
+                                       string entry,
+                                       CancellationToken cancellationToken)
     {
         await _distributedCacheProvider.SetRowAsync(collectionKey, key, entry, cancellationToken);
     }
