f2bba5ee38c647cce1327200b2b84228da767b6f
Kévin ROBERT <kevin.robert.kr@gmail.com>
2024-01-22 13:09:49 +0100

    maj

--- tools/scripts/nuget-list-csproj.ps1
+++ tools/scripts/nuget-list-csproj.ps1
@@ -1,32 +1,85 @@
 $template = Get-Content -Path ./tools/scripts/nuget-template-pipeline.yml
 
+
+
+function Testss( $context  ) { 
+
+  
+  
+
+  # Write-Host =========== $context.BaseName
+ 
+  
+
+
+  Get-Content $context.FullName `
+| Find "<ProjectReference Include=" `
+| ForEach-Object { $_ -replace '<ProjectReference Include=', '' -replace '/>', '' }  `
+| Sort-Object -Unique  `
+| ForEach-Object {
+  
+    
+   
+    $ddd = $PSItem.trim().replace("..\", '').replace('"', '')
+
+
+    # Write-Host $ddd
+    
+    $outputPath = Join-Path ".\src\" $ddd  
+      
+    # Test-Path $outputPath
+
+
+    $ssd = Get-Item $outputPath 
+ 
+
+    $global:array += $ssd.BaseName
+
+    Testss (Get-Item $outputPath  )
+
+
+  }
+
+ 
+  # Write-Host ===========
+  # Write-Host  
+
+   
+}
+
+
 (Get-ChildItem ./src -Filter *.csproj -Recurse) | ForEach-Object {
-  Write-Host =========== $_.BaseName
+ 
+  # if (  $_.BaseName.StartsWith( "Krosoft.Extensions.Data.EntityFramework." )
+  # ) {
+
+
   $fileName = "./tools/azure-pipelines/nuget-$($_.BaseName)-pipeline.yml"
   New-Item $fileName -Force  
- 
-  $array = @($_.BaseName)
 
-  Get-Content $_.FullName `
-  | Find "<ProjectReference Include=" `
-  | ForEach-Object { $_ -replace '<ProjectReference Include=', '' -replace '/>', '' }  `
-  | Sort-Object -Unique  `
-  | Split-Path -Leaf   `
-  | ForEach-Object { $array += $_.replace('.csproj"', '') } 
-  
+
+  # Write-Host =========== $_.BaseName
+      
+  $global:array = @($_.BaseName)
+
+ 
+  Testss $_  
+
+  # Write-Host $global:array
+  # Write-Host $global:array.Count
+
 
   $current = $template
   $current = $current.replace('KRO_PACKAGE_NAME', $_.BaseName)
- 
+   
   $sb = [System.Text.StringBuilder]::new()
-  $array | ForEach-Object { [void]$sb.Append( '     - ' )
+  $array | Sort-Object | Get-Unique | ForEach-Object { [void]$sb.Append( '     - ' )
     [void]$sb.AppendLine( '"' + $PSItem.replace(' ', '') + '"' ) }
-
- 
+  
+   
   $current = $current.replace('KRO_PACKAGES', $sb.ToString() )
-  
+    
   Set-Content $fileName $current
-  
-  Write-Host ===========
-  Write-Host  
-}   
+ 
+} 
+# }
