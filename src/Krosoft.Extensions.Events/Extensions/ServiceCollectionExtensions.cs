9439ce9f3b4b779e6bceb1eb9b8ced888af34365
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-08-20 10:00:17 +0200

    fix

--- src/Krosoft.Extensions.Events/Extensions/ServiceCollectionExtensions.cs
+++ src/Krosoft.Extensions.Events/Extensions/ServiceCollectionExtensions.cs
@@ -1,6 +1,6 @@
 ﻿using Krosoft.Extensions.Events.Interfaces;
 using Krosoft.Extensions.Events.Services;
-
+using Krosoft.Extensions.Jobs.Extensions;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Krosoft.Extensions.Events.Extensions;
@@ -8,10 +8,6 @@ namespace Krosoft.Extensions.Events.Extensions;
 public static class ServiceCollectionExtensions
 {
     public static IServiceCollection AddEvents(this IServiceCollection services)
-    {
-        services.AddFireForget();
-        services.AddTransient<IEventService, EventService>();
-
-        return services;
-    }
+        => services.AddFireForget()
+                   .AddTransient<IEventService, EventService>();
 }
