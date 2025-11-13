f281b76c0866ab56ff7c5dfa5bd860d08bd8e383
Kévin ROBERT <kevin.robert.kr@gmail.com>
2025-11-13 04:28:43 +0100

    Prise en charge .Net 10

---
+++ samples/Krosoft.Extensions.Samples.DotNet10.Api/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Krosoft.Extensions.Samples.DotNet9.Api.Modules;
+using Krosoft.Extensions.WebApi.Identity.Extensions;
+
+namespace Krosoft.Extensions.Samples.DotNet9.Api.Extensions;
+
+internal static class WebApplicationExtensions
+{
+    public static WebApplication AddModules(this WebApplication app)
+    {
+        var groupHello = app.MapGroup("/Hello").WithTags("Hello");
+
+        groupHello.MapGet("/", HelloModule.Hello);
+        groupHello.MapGet("/ApiKey", HelloModule.HelloApiKey).RequireApiKey();
+        return app;
+    }
+}
